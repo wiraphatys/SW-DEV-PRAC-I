e6a6aec270109116db635331dc96b6912135970e
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-02-25 02:26:55 +0700

    Added solution for a-7

---
+++ a-7-wiraphatys/routes/hospitalRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const appointmentRouter = require('./appointmentRouter')
+
+const { getHospitals, getHospital, createHospital, updateHospital, deleteHospital } = require('../controllers/hospitalController')
+const { protect, authorize } = require('../middleware/authMiddleware')
+
+router.route('/').get(getHospitals).post(protect, authorize("admin"), createHospital);
+router.route('/:id').get(getHospital).put(protect, authorize("admin"), updateHospital).delete(protect, authorize("admin"), deleteHospital);
+
+router.use('/:hospitalId/appointments', appointmentRouter)
+
+module.exports = router
