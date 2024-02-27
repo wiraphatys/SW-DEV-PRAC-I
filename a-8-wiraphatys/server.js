a5f5a6c80a28481a9c2affd144464b274b2fafad
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-02-27 17:22:49 +0700

    Added solution for a-8

---
+++ a-8-wiraphatys/server.js
@@ -0,0 +1,33 @@
+// define express
+const express = require('express')
+const app = express()
+app.use(express.json())     // use bodyParser
+
+// define port
+const PORT = process.env.PORT || 3000;
+
+// define .env
+const dotenv = require('dotenv')
+dotenv.config({path: "./config/config.env"})
+
+// connect database
+const connectDB = require('./config/db')
+connectDB()
+
+// cookie parser
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
+// router
+app.use("/api/v1/hospitals", require('./routes/hospitalRouter'))
+app.use("/api/v1/auth", require('./routes/authRouter'))
+app.use("/api/v1/appointments", require('./routes/appointmentRouter'))
+
+const server = app.listen(PORT, () => {
+    console.log(`This server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
+
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`)
+    server.close(() => process.exit(1))
+})
